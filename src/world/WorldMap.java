767ad7c98078306f29d51fd9b9fcbef09aaf8560
Jedrzej Krolikowski <jedrzejkrolikowski@MacBook-Air-Jedrzej.local>
2018-12-16 16:26:01 +0100

    Fully functioning prototype

--- src/world/WorldMap.java
+++ src/world/WorldMap.java
@@ -17,7 +17,8 @@ public class WorldMap {
         tutorialRoom.addAction("Get Orb".toLowerCase(),new Integer(1));
         tutorialRoom.setEnemy(new enemy(100,5,"Troll",null));
         map.add(tutorialRoom);
-        map.add(new room("Tutorial Room2",-1,0,-1,-1,"Tutorial Room"));
+        map.add(new room("Tutorial Room2",-1,0,2,-1,"Tutorial Room"));
+        map.add(new room("North 2",-1,-1,-1,1,"East room"));
     }
     public room getCurrentRoom()
     {
